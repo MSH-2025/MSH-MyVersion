a97048108c8af9a2c0a541f9a1446b07447e317d
Lil-P0ly <makskryzh@gmail.com>
2025-03-25 21:42:36 +0500

    The product filter

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlencode
 from django import template
 
 
@@ -10,3 +11,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_paramas(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
