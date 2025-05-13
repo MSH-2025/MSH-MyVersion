9df1d525f3c5416d38795f1d9246964078efde6a
user <user@Ubuntu.myguest.virtualbox.org>
2025-05-13 08:42:11 +0500

    hot fix 13may

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),  # тут добавила
+    path('delivery', views.delivery, name='delivery'),  # тут добавила
 
 ]
